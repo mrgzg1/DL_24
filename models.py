a8ef02bb4950196f10ff979ad1b8b3737c8005e6
pratyaksh10 <praopratyaksh1@gmail.com>
2024-11-29 10:33:54 -0500

    Project template added

---
+++ models.py
@@ -0,0 +1,65 @@
+from typing import List
+import numpy as np
+from torch import nn
+from torch.nn import functional as F
+import torch
+
+
+def build_mlp(layers_dims: List[int]):
+    layers = []
+    for i in range(len(layers_dims) - 2):
+        layers.append(nn.Linear(layers_dims[i], layers_dims[i + 1]))
+        layers.append(nn.BatchNorm1d(layers_dims[i + 1]))
+        layers.append(nn.ReLU(True))
+    layers.append(nn.Linear(layers_dims[-2], layers_dims[-1]))
+    return nn.Sequential(*layers)
+
+
+class MockModel(torch.nn.Module):
+    """
+    Does nothing. Just for testing.
+    """
+
+    def __init__(self, device="cuda", bs=64, n_steps=17, output_dim=256):
+        super().__init__()
+        self.device = device
+        self.bs = bs
+        self.n_steps = n_steps
+        self.repr_dim = 256
+
+    def forward(self, states, actions):
+        """
+        Args:
+            states: [B, T, Ch, H, W]
+            actions: [B, T-1, 2]
+
+        Output:
+            predictions: [B, T, D]
+        """
+        return torch.randn((self.bs, self.n_steps, self.repr_dim)).to(self.device)
+
+
+class Prober(torch.nn.Module):
+    def __init__(
+        self,
+        embedding: int,
+        arch: str,
+        output_shape: List[int],
+    ):
+        super().__init__()
+        self.output_dim = np.prod(output_shape)
+        self.output_shape = output_shape
+        self.arch = arch
+
+        arch_list = list(map(int, arch.split("-"))) if arch != "" else []
+        f = [embedding] + arch_list + [self.output_dim]
+        layers = []
+        for i in range(len(f) - 2):
+            layers.append(torch.nn.Linear(f[i], f[i + 1]))
+            layers.append(torch.nn.ReLU(True))
+        layers.append(torch.nn.Linear(f[-2], f[-1]))
+        self.prober = torch.nn.Sequential(*layers)
+
+    def forward(self, e):
+        output = self.prober(e)
+        return output
