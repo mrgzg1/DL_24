0e3ccabdffaa52d09264c78c28f9ea3b81870b0b
Manan-Jigar Gandhi <mjgandhi@Manan-Jigars-MacBook-Air.local>
2024-12-12 04:50:13 -0500

    push things'

--- sweep.py
+++ sweep.py
@@ -59,24 +59,25 @@ sweep_config = {
     }
 }
 
-def create_sweep():
+def create_sweep(project_name):
     # Initialize sweep
-    sweep_id = wandb.sweep(sweep_config, project='wall_jepa_sweep')
+    sweep_id = wandb.sweep(sweep_config, project=project_name)
     print("Created sweep with ID:")
     print(sweep_id)
     return sweep_id
 
-def start_agent(sweep_id=None):
+def start_agent(project_name, sweep_id=None):
     if sweep_id is None:
-        sweep_id = create_sweep()
+        sweep_id = create_sweep(project_name)
     
     # Start sweep agent
-    wandb.agent(sweep_id, train_jepa, count=20)  # Will run 20 different configurations
+    wandb.agent(sweep_id, train_jepa, count=20, project=project_name)  # Will run 20 different configurations
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--sweep_id', type=str, help='Optional: ID of existing sweep to run')
+    parser.add_argument('--project_name', type=str, default='wall_jepa_sweep', help='W&B project name')
     args = parser.parse_args()
     
-    start_agent(args.sweep_id)
+    start_agent(args.project_name, args.sweep_id)
