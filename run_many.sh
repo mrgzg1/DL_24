dd5e4ba6777d55b3c8e683f2a7a3837cf1b8e340
Ubuntu <ubuntu@ip-172-31-13-201.us-east-2.compute.internal>
2024-12-12 08:26:56 +0000

    rm

--- run_many.sh
+++ run_many.sh
@@ -1,17 +1,4 @@
-# example base we run with:
-# python main.py --experiment_name text_exp --data_path /data/DL_24/data --gpu_id 0 -b 512
-
-
-EXP_NAME=b64_cnn_vr
-python main.py --experiment_name $EXP_NAME --data_path /data/DL_24/data --gpu_id 6 -b 64  >> resources/$EXP_NAME.log 2>&1 &
-
-EXP_NAME=b128_cnn_vr
-python main.py --experiment_name $EXP_NAME --data_path /data/DL_24/data --gpu_id 5 -b 128 >> resources/$EXP_NAME.log 2>&1 &
-
-EXP_NAME=b256_cnn_vr
-python main.py --experiment_name $EXP_NAME --data_path /data/DL_24/data --gpu_id 4 -b 256 >> resources/$EXP_NAME.log 2>&1 &
-
-EXP_NAME=b256_vit_vr
-python main.py --experiment_name $EXP_NAME --data_path /data/DL_24/data --gpu_id 3 -b 1024 --encoder_type vit --loss_type vicreg >> resources/$EXP_NAME.log 2>&1 &
+# set SWEEP_ID and run this script to take over all the available GPU, estimated 10G / agent / GPU
+CUDA_VISIBLE_DEVICES=1 wandb agent -p wall_jepa_sweep $SWEEP_ID
 
 
