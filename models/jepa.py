5396ac0cb6f1a68fde085927a67a87c9c29858a4
Manan-Jigar Gandhi <mjgandhi@Manan-Jigars-MacBook-Air.local>
2024-12-12 02:39:43 -0500

    Revert "REVERT ME, large model"
    This reverts commit b2dbe4403562d27580dbfbd6fb760a9fdf9fb78d.

--- models/jepa.py
+++ models/jepa.py
@@ -35,15 +35,15 @@ from .encoder import Encoder, CNNBackbone
 from .predictor import Predictor
 
 class JEPA(nn.Module):
-    def __init__(self, device="cpu", bs=64, n_steps=17, enc_dim=512, action_dim=2, config=None):
+    def __init__(self, device="cpu", bs=64, n_steps=17, enc_dim=256, action_dim=2, config=None):
         super(JEPA, self).__init__()
         self.device = device
         self.bs = bs
         self.n_steps = n_steps
-        self.repr_dim = enc_dim
+        self.repr_dim = 256
         self.config = config
         # self.encoder = Encoder(n_kernels=4, repr_dim=enc_dim)
-        self.encoder = Encoder(n_kernels=8, repr_dim=enc_dim, config=config)
+        self.encoder = Encoder(n_kernels=4, repr_dim=enc_dim, config=config)
         self.target_encoder = self.get_target_encoder()
         self.predictor = Predictor(enc_dim=enc_dim, action_dim=action_dim, 
                                    arch="1024-512-256", n_steps=n_steps, 
